cefce47df85f4ccc00705075f1a9c3ff9c549e9f
Zigy <zigybass@gmail.com>
2019-08-22 15:33:24 -0400

    route connections. started friends and api routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,8 @@
+
+
+app.get("/api/friends", function (req, res) {
+    res.json(results)
+});
+
+app.post("/api/friends", function (req, res) {})
+    
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-$("document").ready( function () {
-
-const path = require("path");
 const express = require("express");
+const path = require("path");
+const app = express();
 
+const PORT = process.env.PORT || 8080;
 
-});
+app.use(express.static("app/public"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes");
+require("./app/routing/htmlRoutes")(app, path);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
