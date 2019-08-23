065d984702056cd938ba50c41d925e61e14e016e
Zigy <zigybass@gmail.com>
2019-08-22 23:59:17 -0400

    last minute modal

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,24 +1,24 @@
-$("#submitResults").click(function (e) {
-    e.preventDefault()
-    console.log("click")
-    const newPerson = {
-        "name": $("#nameInput").val(),
-        "photo": $("#photoInput").val(),
-        "scores": [
-            $("#q1").val(),
-            $("#q2").val(),
-            $("#q3").val(),
-            $("#q4").val(),
-            $("#q5").val(),
-            $("#q6").val(),
-            $("#q7").val(),
-            $("#q8").val(),
-            $("#q9").val(),
-            $("#q10").val(),
-        ]
-    }
-    console.log(newPerson)
-    $.post("/api/friends", newPerson, function (res, req) {
-        console.log("Person made, post made")
-    })
-})
+// $("#submitResults").click(function (e) {
+//     e.preventDefault()
+//     console.log("click")
+//     const newPerson = {
+//         "name": $("#nameInput").val(),
+//         "photo": $("#photoInput").val(),
+//         "scores": [
+//             $("#q1").val(),
+//             $("#q2").val(),
+//             $("#q3").val(),
+//             $("#q4").val(),
+//             $("#q5").val(),
+//             $("#q6").val(),
+//             $("#q7").val(),
+//             $("#q8").val(),
+//             $("#q9").val(),
+//             $("#q10").val(),
+//         ]
+//     }
+//     console.log(newPerson)
+//     $.post("/api/friends", newPerson, function (res, req) {
+//         console.log("Person made, post made")
+//     })
+// })
